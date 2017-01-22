94c9e1f779759662403104a55747c88950fb2fbc
Ian Munro <ianmunrobot@gmail.com>
2017-01-22 18:45:35 -0500

    closes #3 add view
    -added second view 'coolBlobs'
    -refactored and restructured views

--- app/index.js
+++ app/index.js
@@ -1,17 +1,15 @@
 const Tone = require('tone')
-// const EventEmitter = require('./event-emitter')
+
+const socket = require('./socket')
+const spaceCircle = require('./views/spaceCircle')
+const coolBlobs = require('./views/coolBlobs')
 
 const canvas = document.getElementById('paperCanvas');
 paper.setup(canvas);
 paper.install(window);
 
 // const drums = require('./basic-beat')
-
-var socket = io(window.location.origin)
 let room = window.location.pathname.slice(1);
-
-// put the socket on global scope (required for paperscript file to access and emit events)
-window.socket = socket
 
 // synthesizer memory
 let synthesizers = {}
@@ -50,8 +48,8 @@ socket.on('populateSynths', (ids) => {
   ids.forEach(id => {
     let newXSynth1 = new Tone.DuoSynth({harmonicity: 1.5}).chain(reverb)
     let newYSynth2 = new Tone.DuoSynth({harmonicity: 1.5}).chain(reverb)
-    newXSynth1.volume.value = -12;
-    newYSynth2.volume.value = -12;
+    newXSynth1.volume.value = -15;
+    newYSynth2.volume.value = -15;
     synthesizers[id] = {
       x: newXSynth1,
       y: newYSynth2
@@ -61,7 +59,7 @@ socket.on('populateSynths', (ids) => {
 
 socket.on('mouseDown', (event) => {
   changeFrequency(event.id, 'x', event.x)
-  changeFrequency(event.id, 'y', event.x * 1.5)
+	changeFrequency(event.id, 'y', event.x * 1.5)
   attack(event.id)
 })
 
@@ -69,7 +67,6 @@ socket.on('mouseDrag', (event) => {
   // console.log(event);
   let amplitude = Math.abs(event.delta[1]) + Math.abs(event.delta[2])
   changeAmplitude(event.id, amplitude)
-
   changeFrequency(event.id, 'x', event.x)
   changeFrequency(event.id, 'y', event.x * 1.5)
 })
@@ -78,67 +75,11 @@ socket.on('mouseUp', (event) => {
   release(event.id)
 })
 
-
-// The amount of circles we want to make:
-var count = 100;
-
-// Space Circle Path:
-var circlePath = new Path.Circle({
-	center: [0, 0],
-	radius: 10,
-	fillColor: 'white',
-	strokeColor: 'white',
-	shadowColor: 'cyan',
-	shadowBlur: 10,
-	shadowOffset: [(Math.floor(Math.random() * 4) - 2), (Math.floor(Math.random() * 4) - 2)]
-});
-
-var symbol = new Symbol(circlePath);
-
-// Place the instances of the symbol:
-for (var i = 0; i < count; i++) {
-	// The center position is a random point in the view:
-  var center = new Point((Math.random() * view.size.width), Math.random() * view.size.height);
-	var placedSymbol = symbol.place(center);
-	// symbol.fillColor.hue += (Math.random() * 8)
-	placedSymbol.scale(i / count);
-	placedSymbol.direction = 2 * Math.PI * Math.random()
-}
-
-// The onFrame function is called up to 60 times a second:
-view.onFrame = (event) => {
-	// Run through the active layer's children list and change
-	// the position of the placed symbols:
-	for (var i = 0; i < count; i++) {
-		var item = project.activeLayer.children[i];
-
-		// Move the item 1/20th of its width. This way
-		// larger circles move faster than smaller circles:
-		var speed = item.bounds.width / 30;
-		item.position.x += Math.cos(item.direction) * speed;
-		item.position.y += Math.sin(item.direction) * speed;
-
-		// If the item has the view, move it back
-		if (item.bounds.left > view.size.width) {
-			item.position.x = item.bounds.width;
-		}
-		if (item.bounds.right < 0) {
-			item.position.x = view.size.width
-		}
-		if (item.bounds.bottom < 0) {
-			item.position.y = view.size.height
-		}
-		if (item.bounds.top > view.size.height) {
-			item.position.y = 0
-		}
-	}
-}
+// drawing tool
 
 var tool = new Tool()
-tool.minDistance = 1;
-tool.maxDistance = 45;
-
-var paths = {}
+tool.minDistance = 10;
+tool.maxDistance = 30;
 
 tool.onMouseDown = (event) => {
 	var outEvent = {
@@ -148,34 +89,6 @@ tool.onMouseDown = (event) => {
 	}
   socket.emit('mouseDown', outEvent);
 }
-
-// create drawing paths
-socket.on('mouseDown', function(event) {
-	// console.log(event);
-  var shadow = new Path();
-	var path = new Path();
-	path.fillColor = {
-		hue: Math.random() * 360,
-		saturation: 1,
-		brightness: 1,
-	};
-  shadow.fillColor = {
-		hue: path.fillColor.hue,
-		saturation: 0.5,
-		brightness: 0.8,
-	};
-	path.add(new Point(event.x, event.y));
-  shadow.add(new Point(event.x, event.y));
-	if (!paths[event.id]) {
-		paths[event.id] = {
-			past: []
-		}
-	}
-  paths[event.id].currentPath = {
-    path: path,
-    shadow: shadow
-  };
-})
 
 tool.onMouseDrag = (event) => {
 	var outEvent = {
@@ -188,71 +101,13 @@ tool.onMouseDrag = (event) => {
   socket.emit('mouseDrag', outEvent)
 }
 
-socket.on('mouseDrag', function(event) {
-	var middlePoint = new Point(event.middlePoint[1], event.middlePoint[2])
-	var step = new Point(event.delta[1] / 2, event.delta[2] / 2)
-	step.angle += 90;
-
-  // add new paths
-  var top = new Point(middlePoint.x + step.x, middlePoint.y + step.y)
-	var bottom = new Point(middlePoint.x - step.x, middlePoint.y - step.y)
-
-  // shadow paths
-  var shadowTop = new Point(middlePoint.x + (step.x * 1.5), middlePoint.y + (step.y * 2))
-	var shadowBottom = new Point(middlePoint.x - (step.x * 1.5), middlePoint.y - (step.y * 2))
-
-	var currentPath = paths[event.id].currentPath;
-	currentPath.path.add(top);
-	currentPath.path.insert(0, bottom);
-	currentPath.path.smooth();
-
-  currentPath.shadow.add(shadowTop);
-  currentPath.shadow.insert(0, shadowBottom);
-  currentPath.shadow.smooth();
-
-})
-
 tool.onMouseUp =(event) => {
 	var outEvent = {
 		id: socket.id,
 		x: event.point.x,
 		y: event.point.y,
 	}
-
   socket.emit('mouseUp', outEvent)
 }
 
-socket.on('mouseUp', function(event) {
-	var current = paths[event.id].currentPath.path;
-  var shadow = paths[event.id].currentPath.shadow;
-	current.add(new Point(event.x, event.y));
-  shadow.add(new Point(event.x, event.y));
-
-	current.closed = true;
-	shadow.closed = true;
-
-  current.smooth();
-  shadow.smooth();
-
-	paths[event.id].past.push(current)
-  paths[event.id].past.push(shadow)
-
-	timedRemove(current, event.id)
-  timedRemove(shadow, event.id)
-	delete paths[event.id].currentPath
-})
-
-
-var timedRemove = function(pathToRemove, id) {
-	var alpha = 1.0
-	var fadeOut = window.setInterval(function() {
-		if (alpha === 0) {
-			window.clearInterval(fadeOut)
-			pathToRemove.remove()
-			paths[id].past.unshift()
-		} else {
-			pathToRemove.fillColor.alpha -= 0.05
-		}
-	}, 50)
-}
-
+coolBlobs();
