0708141da952bfb9e03ccaefbf1a6cf939801219
Ian Munro <ianmunrobot@gmail.com>
2017-01-22 23:09:35 -0500

    architecture refactor
    refactored synth and paths memory
    
    enabled view switching
    
    added comments

--- app/index.js
+++ app/index.js
@@ -1,38 +1,51 @@
 const Tone = require('tone')
 
 const socket = require('./socket')
+
 const spaceCircle = require('./views/spaceCircle')
 const coolBlobs = require('./views/coolBlobs')
 
-const synths = require('./toneCenter').start
-const duoSynth = require('./toneCenter').duoSynth
+const start = require('./toneCenter').start
+const duoSynth = require('./instruments/duoSynth');
 
 const views = {
 	spaceCircle,
 	coolBlobs
 }
 
-const synthesizers = {}
+// main synth memory
+const synthesizers = {};
 
-synths(synthesizers)
+// start with audio defaults
+start();
+duoSynth(synthesizers)
+const drums = require('./basic-beat')
 
+// menu click handler
 $('a').on('click', function(e) {
+	// update menu
+	$('li').removeClass('active')
+	$(this).parent().addClass('active')
 	e.preventDefault()
+	// reset socket listeners
 	socket.removeAllListeners('serverDown');
 	socket.removeAllListeners('serverDrag');
 	socket.removeAllListeners('serverUp');
+
+	// reset and repopulate view
 	project.clear();
 	views[this.id]();
+
+	// restart synth
 	duoSynth(synthesizers);
 })
-
-const drums = require('./basic-beat')
 
 // set up canvas
 const canvas = document.getElementById('paperCanvas');
 paper.setup(canvas);
 paper.install(window);
 
+// begin view default
 spaceCircle();
 
 let room = window.location.pathname.slice(1);
@@ -42,7 +55,7 @@ socket.on('connect', () => {
 	socket.emit('create', room);
 });
 
-// drawing tool
+// drawing tool emitters
 var tool = new Tool()
 tool.minDistance = 10;
 tool.maxDistance = 30;
