8ff64ce113d926bb7f90bf0a2a7db0d187d0eb27
Ian Munro <ianmunrobot@gmail.com>
2017-01-23 12:07:04 -0500

    added start/stop func

--- app/grounds/spacey.js
+++ app/grounds/spacey.js
@@ -6,11 +6,14 @@ var chorus = new Tone.Chorus(0.02, 2.5, 0.5).connect(reverb)
 var chorus2 = new Tone.Chorus(0.03, 2.5, 0.5).connect(reverb)
 var chorus3 = new Tone.Chorus(0.04, 2.5, 0.5).connect(reverb)
 
-var piano = new Tone.FMSynth({
+var gain = new Tone.Gain(0.6)
+
+var wanderSynth = new Tone.FMSynth({
 			"modulationIndex" : 1,
 			"envelope" : {
 				"attack" : 1,
-				"decay" : 0.2
+				"decay" : 0.2,
+				'release': 1
 			},
 			"modulation" : {
 				"type" : "triangle"
@@ -19,15 +22,16 @@ var piano = new Tone.FMSynth({
 				"attack" : 0.2,
 				"decay" : 0.01
 			},
-      "volume": -10,
+      "volume": -15,
       'portamento': 0.2
-    }).connect(chorus);
+    }).chain(chorus);
 
-var piano2 = new Tone.FMSynth({
+var wanderSynth2 = new Tone.FMSynth({
   "modulationIndex" : 3.2,
   "envelope" : {
     "attack" : 1,
-    "decay" : 0.2
+    "decay" : 0.2,
+		'release': 1
   },
   "modulation" : {
     "type" : "triangle"
@@ -36,11 +40,11 @@ var piano2 = new Tone.FMSynth({
     "attack" : 0.2,
     "decay" : 0.01
   },
-  "volume": -12,
+  "volume": -18,
   'portamento': 0.2
 }).connect(chorus2);
 
-var drone = new Tone.FMSynth({
+var drone1 = new Tone.FMSynth({
 			"modulationIndex" : 3,
 			"envelope" : {
 				"attack" : 4,
@@ -53,9 +57,8 @@ var drone = new Tone.FMSynth({
 				"attack" : 0.2,
 				"decay" : 0.01
 			},
-    }).connect(chorus3);
+    }).chain(gain, chorus3);
 
-drone.triggerAttack("F2")
 
 var drone2 = new Tone.FMSynth({
 			"modulationIndex" : 2,
@@ -70,37 +73,54 @@ var drone2 = new Tone.FMSynth({
 				"attack" : 0.2,
 				"decay" : 0.01
 			}
-    }).connect(chorus3);
+    }).chain(gain, chorus3);
 
-drone2.triggerAttack('C3', "2m")
 
-		// var pianoPart = new Tone.Part(function(time, chord){
 
-		// 	piano.triggerAttackRelease(chord, "8n", time);
-		// }, [["0:0:2", 'C1'], ["0:1", 'D2'], ["0:1:3", 'E2'], ["0:2:2", "D2"], ["0:3", 'C2'], ["0:3:2", 'F2']]).start(0);
-		// pianoPart.loop = true;
-		// pianoPart.loopEnd = "1m";
 
 var scale = ["F3", "G3", "A4", "Bb3", "C3", "D3", "Eb3"]
 var scale2 = ["F3", "G3", "A4", "C4", "D4", "Eb4", "F4"]
 
-//play a note every eighth note starting from the first measure
-Tone.Transport.scheduleRepeat(function(time){
+
+var wanderSynthSchedule
+var wanderSynth2Schedule
+
+
+function start() {
+	drone1.triggerAttack("F2")
+	drone2.triggerAttack('C3', "2m")
+
+	wanderSynthSchedule = Tone.Transport.scheduleRepeat(function(time){
   if (Math.random() < 0.07){
     var nextNote = scale[Math.floor(Math.random() * (scale.length - 1))]
-    piano.triggerAttack(nextNote)
-    // piano.triggerRelease(time);
+    wanderSynth.triggerAttack(nextNote)
   }
 }, "8n", "3m");
 
-Tone.Transport.scheduleRepeat(function(time){
+	wanderSynth2Schedule = Tone.Transport.scheduleRepeat(function(time){
   if (Math.random() < 0.1){
     var nextNote = scale2[Math.floor(Math.random() * (scale2.length - 1))]
-    piano2.triggerAttack(nextNote)
-    // piano.triggerRelease(time);
+    wanderSynth2.triggerAttack(nextNote)
   }
 }, "8n", "4m");
 
-Tone.Transport.bpm.value = 72;
+	Tone.Transport.bpm.value = 72;
+	Tone.Transport.start("+0.1")
 
-Tone.Transport.start("+0.1")
+}
+
+function stop() {
+	drone1.triggerRelease()
+	drone2.triggerRelease()
+	drone2.frequency.value = 0;
+	drone2.triggerRelease('2m')
+	wanderSynth.triggerRelease()
+	wanderSynth2.triggerRelease()
+	Tone.Transport.clear(wanderSynthSchedule)
+	Tone.Transport.clear(wanderSynth2Schedule)
+}
+
+module.exports = {
+	start: start,
+	stop: stop
+}
